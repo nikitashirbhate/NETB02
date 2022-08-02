5ca64847f65b4ef431981255111d3a895bb6090b
nikitashirbhate <110405848+nikitashirbhate@users.noreply.github.com>
2022-08-02 20:12:26 +0530

    by nikita

--- NETB02_GIT/src/Demo.java
+++ NETB02_GIT/src/Demo.java
@@ -8,6 +8,9 @@ public class Demo
 	public void m2()
 	{
 		System.out.println("m2---method");
+	}public void m3()
+	{
+		System.out.println("m3---method");
 	}
 public static void main(String[] args) 
 {
