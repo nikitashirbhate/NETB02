b0ea66086ff0cb0a53aa85b79e4fb66020a313e2
nikitashirbhate <110405848+nikitashirbhate@users.noreply.github.com>
2022-08-02 20:02:49 +0530

    updated by nikita

--- NETB02_GIT/src/Demo.java
+++ NETB02_GIT/src/Demo.java
@@ -1,6 +1,12 @@
 
-public class Demo {
-public static void main(String[] args) {
+public class Demo
+{
+	public void m1()
+	{
+		System.out.println("m1---method");
+	}
+public static void main(String[] args) 
+{
 	System.out.println("hello");
 }
 }
